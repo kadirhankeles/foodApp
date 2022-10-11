68d9f652459b9e0b31889e1f946864448b200677
Kadirhan Keles <kadirhankeles@gmail.com>
2022-10-11 13:41:45 +0300

    Login sayfasının tasarımı bitirildi.

--- lib/pages/loginScreen.dart
+++ lib/pages/loginScreen.dart
@@ -14,7 +14,23 @@ class _LoginScreenPageState extends State<LoginScreenPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: loginButton(),
+      backgroundColor: Color(0xff010E16),
+      body: Column(
+        children: [
+          Image.asset("assets/arkaplan.png"),
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Text('Lorem ipsum \ndolor sit amet,\nconsectetur adipiscing elit.  ', style: TextStyle(color: Colors.white, fontSize: 38, fontWeight: FontWeight.bold),),
+          ),SizedBox(height: 60,),
+          loginButton(buttonColor:Colors.white,buttonText: "Login"),
+          SizedBox(height: 30,),
+          Text('Don’t have an account? ', style: TextStyle(color: Color(0xff375268), ),),
+          Text('Create account', style: TextStyle(color: Color(0xffD9D9D9)),)
+        ],
+      ),
+      
+      
     );
   }
 }
