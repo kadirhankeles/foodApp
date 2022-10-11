ab408353c97e44ccd33726863365ef359bd2432a
Kadirhan Keles <kadirhankeles@gmail.com>
2022-10-11 10:27:09 +0300

    loginButton tasarımı gerçekleştirildi.

--- lib/widgets/loginButton.dart
+++ lib/widgets/loginButton.dart
@@ -10,13 +10,22 @@ class loginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 63,
-        width: 360,
-        
-        child: Text('deneme'),
-        margin: EdgeInsets.only(left: 18, right: 18),
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color(0xff010E16),),
+      body: Column(
+        children: [
+          Container(
+            height: 63,
+            width: 360,
+            child: TextButton(onPressed: (() {
+              
+            }), child: Center(child: Text('Login', style: TextStyle(color: Color(0xffF5F5F5), fontSize: 29),)),),
+            margin: EdgeInsets.only(left: 18, right: 18),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color(0xff010E16), border: Border.all(width: 1 ,color: Color(0xff314B61))),
+            
+          ),
+          SizedBox(height: 30,),
+          Text('Don’t have an account? ', style: TextStyle(color: Color(0xff375268), fontSize: 14),),
+          Text('Create Account', style: TextStyle(color: Colors.white),),
+        ],
       ),
     );
   }
