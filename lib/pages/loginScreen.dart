e4e3646afea32cf262cfb872814fa028d040f608
Kadirhan Keles <kadirhankeles@gmail.com>
2022-10-11 11:43:58 +0300

    develop|| Buton develop'a eklendi

---
+++ lib/pages/loginScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:foodapplication/widgets/loginButton.dart';
+
+class LoginScreenPage extends StatefulWidget {
+  const LoginScreenPage({super.key});
+
+  @override
+  State<LoginScreenPage> createState() => _LoginScreenPageState();
+}
+
+class _LoginScreenPageState extends State<LoginScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: loginButton(),
+    );
+  }
+}
