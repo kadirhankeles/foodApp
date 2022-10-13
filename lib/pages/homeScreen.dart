6697a94084bf97fda1a79f50c2cd3759fa415665
Kadirhan Keles <kadirhankeles@gmail.com>
2022-10-13 13:37:29 +0300

    Restaurant kısmı eklendi.

--- lib/pages/homeScreen.dart
+++ lib/pages/homeScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:foodapplication/widgets/banner.dart';
 import 'package:foodapplication/widgets/categories.dart';
+import 'package:foodapplication/widgets/restaurant.dart';
 import 'package:foodapplication/widgets/searchBar.dart';
 import 'package:foodapplication/widgets/userLocation.dart';
 
@@ -16,11 +17,11 @@ class homeScreen extends StatefulWidget {
 class _homeScreenState extends State<homeScreen> {
   @override
   final List<String> categoriesImage = [
-    "assets/homeScreen/Ellipse 1.png",
-    "assets/homeScreen/Ellipse 2.png",
-    "assets/homeScreen/Ellipse 3.png",
-    "assets/homeScreen/Ellipse 4.png",
-    "assets/homeScreen/Ellipse 5.png",
+    "assets/homeScreen/pizza1.jpg",
+    "assets/homeScreen/pancake.jpg",
+    "assets/homeScreen/sandvic.jpg",
+    "assets/homeScreen/dondurma.jpg",
+    "assets/homeScreen/noodle.jpg",
   ];
   final List<String> categoriesName = [
     "Pizza",
@@ -29,64 +30,140 @@ class _homeScreenState extends State<homeScreen> {
     "Ice-cream",
     "Noodle",
   ];
+
+  final List<String> restaurantImage = [
+    "assets/homeScreen/Rectangle 8.png",
+    "assets/homeScreen/Rectangle 14 (2).png",
+    "assets/homeScreen/Rectangle 17.png",
+  ];
+  final List<String> restaurantName = [
+    "Burger King",
+    "Pizzania",
+    "Pancake",
+  ];
+  final List<double> restaurantStar = [
+    4.5,
+    4.6,
+    4.2,
+  ];
+  final List<String> restaurantTime = [
+    "25-35 mins",
+    "20-25 mins",
+    "25-35 mins",
+  ];
+  final List<String> restaurantDistance = [
+    "8 km",
+    "7 km",
+    "8 km",
+  ];
+  final List<bool> isNew = [
+    true,
+    false,
+    true,
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          userLocation(isim: "Kadirhan", konum: "Istanbul"),
-          SizedBox(
-            height: 20,
-          ),
-          searchBar(),
-          SizedBox(
-            height: 20,
-          ),
-          banner(),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Categories',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            userLocation(isim: "Kadirhan", konum: "Istanbul"),
+            SizedBox(
+              height: 20,
+            ),
+            searchBar(),
+            SizedBox(
+              height: 20,
+            ),
+            banner(),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
                   ),
-                ),
-                Text(
-                  'Show all',
-                  style: TextStyle(fontSize: 12, color: Color(0xff459FCA)),
-                ),
-              ],
+                  Text(
+                    'Show all',
+                    style: TextStyle(fontSize: 12, color: Color(0xff459FCA)),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            width: double.infinity,
-            height: 100,
-           
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: categoriesImage.length,
-              itemBuilder: (context, index) {
-              return Container(
-               
-                width: 90,
-                child: categories(categoriesImage: categoriesImage[index], categoriesName: categoriesName[index]),
-              );
-              },
+            SizedBox(
+              height: 10,
             ),
-          )
-        ],
+            Container(
+              width: double.infinity,
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categoriesImage.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 90,
+                    child: categories(
+                        categoriesImage: categoriesImage[index],
+                        categoriesName: categoriesName[index]),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Restaurant',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    'Show all',
+                    style: TextStyle(fontSize: 12, color: Color(0xff459FCA)),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 380,
+              width: double.infinity,
+              child: ListView.builder(
+                physics: ScrollPhysics(),
+                itemCount: restaurantImage.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                      margin: index > 0
+                          ? EdgeInsets.only(top: 20)
+                          : EdgeInsets.only(top: 0),
+                      child: restaurant(
+                          restaurantImage: restaurantImage[index],
+                          restaurantName: restaurantName[index],
+                          restaurantStar: restaurantStar[index],
+                          restaurantTime: restaurantTime[index],
+                          restaurantDistance: restaurantDistance[index],
+                          isNew: isNew[index]));
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
