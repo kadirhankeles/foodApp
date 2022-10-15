4b5faf7cebab58a5b1fd9da7018b129a1cdcdce6
Kadirhan Keleş <kadirhankeles@gmail.com>
2022-10-15 17:19:45 +0300

    Selection'da buton tıklama aktif hale getirildi

--- lib/widgets/selection.dart
+++ lib/widgets/selection.dart
@@ -12,13 +12,29 @@ class selectionFood extends StatefulWidget {
 }
 
 class _selectionFoodState extends State<selectionFood> {
+bool isClicked = false;
+int clickCount =0;
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 30,
       width: 82,
-      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(5),border: Border.all(color: Colors.black)),
-      child: Center(child: Text('${widget.food}')),
+      decoration: BoxDecoration(color: isClicked==true?Colors.black :Colors.white, borderRadius: BorderRadius.circular(5),border: Border.all(color: Colors.black)),
+      child: Center(child: 
+      TextButton(
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        onPressed: () {
+          setState(() {
+            clickCount++;
+            if(clickCount%2!=0){
+              isClicked = true;
+            }else{isClicked=false;}
+          });
+        
+        print(isClicked);
+      }, child: Text('${widget.food}',style: TextStyle(color: isClicked?Colors.white:Colors.black),))
+      
+      ),
     );
   }
 }
